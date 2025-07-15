9570f44eca37b03084f59b4c3d9a59225c24aee2
kmandt <karsten.mandt@smail.th-koeln.de>
2025-07-15 23:04:38 +0200

    TEST 24 hour format

--- engine/rtc/print_hours_mins.asm
+++ engine/rtc/print_hours_mins.asm
@@ -14,53 +14,32 @@ Function1c0a0d: ; unreferenced
 	ret
 
 PrintHoursMins:
-; Hours in b, minutes in c
-	ld a, b
-	cp 12
-	push af
-	jr c, .AM
-	jr z, .PM
-	sub 12
-	jr .PM
-.AM:
-	or a
-	jr nz, .PM
-	ld a, 12
-.PM:
-	ld b, a
-; Crazy stuff happening with the stack
-	push bc
-	ld hl, sp+1
-	push de
-	push hl
-	pop de
-	pop hl
-	ld [hl], " "
-	lb bc, 1, 2
-	call PrintNum
-	ld [hl], ":"
-	inc hl
-	ld d, h
-	ld e, l
-	ld hl, sp+0
-	push de
-	push hl
-	pop de
-	pop hl
-	lb bc, PRINTNUM_LEADINGZEROS | 1, 2
-	call PrintNum
-	pop bc
-	ld de, String_AM
-	pop af
-	jr c, .place_am_pm
-	ld de, String_PM
-.place_am_pm
-	inc hl
-	call PlaceString
-	ret
+    push bc
+    ld hl, sp+1
+    push de
+    push hl
+    pop de
+    pop hl
+    lb bc, PRINTNUM_LEADINGZEROS | 1, 2
+    call PrintNum
 
-String_AM: db "AM@"
-String_PM: db "PM@"
+    ; Doppelpunkt drucken
+    ld [hl], ":"
+    inc hl
+
+    ; Minuten drucken (zweistellig, mit führender Null)
+    ld d, h
+    ld e, l
+    ld hl, sp+0
+    push de
+    push hl
+    pop de
+    pop hl
+    lb bc, PRINTNUM_LEADINGZEROS | 1, 2
+    call PrintNum
+
+    pop bc
+    ret
 
 PrintHoursMins2:
 ; Hours in b, minutes in c
