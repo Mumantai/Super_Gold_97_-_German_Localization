82138cd8cbf1cc4a18e527b972133d23992a8923
kmandt <karsten.mandt@smail.th-koeln.de>
2025-07-15 20:18:08 +0200

    Fix overflow prevention logic in menu.asm and adjust menu border right coordinate calculation.

--- home/menu.asm
+++ home/menu.asm
@@ -392,13 +392,6 @@ _YesNoBox::
 	ld hl, YesNoMenuHeader
 	call CopyMenuHeader
 	pop bc
-; This seems to be an overflow prevention,
-; but it was coded wrong.
-    ld a, b
-    cp SCREEN_WIDTH - 6
-    jr nz, .okay ; should this be "jr nc"?
-    ld a, SCREEN_WIDTH - 7
-    ld b, a
 
 	ld a, b
 	ld [wMenuBorderLeftCoord], a
@@ -432,7 +425,7 @@ InterpretTwoOptionMenu::
 
 YesNoMenuHeader::
 	db MENU_BACKUP_TILES ; flags
-	menu_coords 10, 5, 16, 9
+	menu_coords 10, 5, 15, 9
 	dw .MenuData
 	db 1 ; default option
 
